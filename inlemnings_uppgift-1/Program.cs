dfda16945ca2b8c86cabe6906c3ada8bd256bb40
maib07001 <maib07001@E-PW05C37V>
2024-10-10 14:01:05 +0200

    Add project files.

---
+++ inlemnings_uppgift-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Exempel_1._1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /\"\"  \\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
